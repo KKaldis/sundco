86dc2198cfbffc80f50c7d4b99f8fa6f128caeba
Kostas Kaldis <kostaskaldis@gmail.com>
2022-11-01 22:42:38 +0200

    clean up css impovments

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,10 +1,6 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
@@ -28,3 +24,4 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
